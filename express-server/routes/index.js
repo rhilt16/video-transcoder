e8aab4c0fd93d720533013be8f7934a6119149bf
rhilt16 <162086587+rhilt16@users.noreply.github.com>
2024-08-25 14:20:29 +1000

    Update index.js

--- express-server/routes/index.js
+++ express-server/routes/index.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post("/upload", (req, res) => {
+  // Check if file is not available return message with status 400.
+  if (req.files === null) {
+    return res.status(400).json({ msg: "No file uploaded" });
+  }
+  const file = req.files.file;
+  // We need unique file name to save it in folder and then use filename to access it. I have replace space with - and concatinated file name with Date String. We can also used uuid package as well.
+  const UFileName = `${new Date().getTime()}-${file.name.replaceAll(" ", "-")}`;
+  // This line of code will save our file in public/uploads folder in our
+  //appliction and will retrun err if any error found if no error found then return pathname of file.
+  file.mv(`${__dirname}/client/public/uploads/${UFileName}`, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
+    res.json({ fileName: UFileName, filePath: `/uploads/${UFileName}` });
+  });
+});
+
+
+
 module.exports = router;

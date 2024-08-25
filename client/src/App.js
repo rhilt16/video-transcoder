d69ee492da25ad8b6af4ddabbd0217281b5b2eb1
rhilt16 <162086587+rhilt16@users.noreply.github.com>
2024-08-25 14:17:14 +1000

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+// Import axios to post Request
+import axios from 'axios'
+// Create State for variables
+import { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Upload from "./upload.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <Router>
+            
+            <Routes>
+                <Route exact path="/upload" element={<Upload />} />
+
+            </Routes>
+        </Router>
+        
+        </div>
+    
+);
 }
 
 export default App;

ff1ea101789764cae1bee91fa20d8a424c509e71
rhilt16 <n11547464@qut.edu.au>
2024-08-26 12:40:06 +0000

    user routes added

--- express-server/controllers/userController.js
+++ express-server/controllers/userController.js
@@ -83,15 +83,84 @@ exports.user_create_post = [
 
 //Handle Author delete on POST.
 exports.user_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author delete POST");
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(400).json({ error: "Invalid user ID" });
+  }
+
+  const user = await User.findById(req.params.id)
+    .exec();
+
+  if (user == null) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+
+  await User.findByIdAndDelete(req.params.id);
+  return res.status(200).send()
+
 });
 
-// Display Author update form on GET.
-exports.user_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author update GET");
-});
 
-// Handle Author update on POST.
-exports.user_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author update POST");
-});
+
+exports.user_update_post = [
+  // Validate and sanitize fields.
+  body("firstName")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("familyName")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified.")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters."),
+  body("email")
+    .isLength({min: 1})
+    .isEmail(),
+  body("password")
+    .isLength({min: 6}),
+
+  //Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Check if the provided ID is a valid ObjectId
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({ error: "Invalid user ID" });
+    }
+
+    // Check if the provided ID is a valid ObjectId
+    const userExists = await User.exists({ _id: req.params.id });
+    if (!userExists) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    // Create Author object with escaped and trimmed data (and the old id!)
+    const user = new User({
+      firstName: req.body.firstName,
+      familyName: req.body.familyName,
+      email: req.body.email,
+      password: req.body.password,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values and error messages.
+      res.status(400).json({
+        user: user,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid. Update the record.
+      await User.findByIdAndUpdate(req.params.id, user);
+      const newuser = await User.findById(req.params.id);
+      res.status(200);
+      res.json(newuser);
+    }
+  }),
+];

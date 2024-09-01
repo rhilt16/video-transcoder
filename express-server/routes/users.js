06337b2e9606cb1d42e1708b5d8eb84d2217bc74
rhilt16 <162086587+rhilt16@users.noreply.github.com>
2024-09-01 21:42:17 +1000

    Comment users.js

--- express-server/routes/users.js
+++ express-server/routes/users.js
@@ -4,23 +4,16 @@ const asyncHandler = require("express-async-handler");
 const user_controller = require("../controllers/userController")
 const JWT = require("../jwt.js");
 
+// Routes all use JWT authentication, and are handled in the upload controller
 
-/* GET users listing. */
 router.get("/", user_controller.user_list);
 
-
-// POST request for creating Author.
 router.post("/create", JWT.authenticateToken, user_controller.user_create_post);
 
-
-// POST request to delete Author.
 router.post("/delete/:id", JWT.authenticateToken, user_controller.user_delete_post);
 
-
-// POST request to update Author.
 router.post("/update/:id/", JWT.authenticateToken, user_controller.user_update_post);
 
-// GET request for one Author.
 router.get("/select/:id", JWT.authenticateToken, user_controller.user_select);
 
 router.get("/uploads/:id", JWT.authenticateToken, user_controller.upload_list);

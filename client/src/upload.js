ebd0c565134d91d3a0d78451b1a56de75680f80f
Ubuntu <ubuntu@ip-172-31-124-157.ap-southeast-2.compute.internal>
2024-09-01 07:16:35 +0000

    More role seperation

--- client/src/upload.js
+++ client/src/upload.js
@@ -7,6 +7,7 @@ function Upload() {
     const [uploadedFile, setUploadedFile] = useState({});
     const [message, setMessage] = useState("");
     const [uploadData, setUploadData] = useState([]);
+    const [userData, setUserData] = useState([]);
 
     let authToken = '';
     let isAdmin = '';
@@ -89,25 +90,99 @@ function Upload() {
         getUploads();
     }, []);
 
+// Fetch User data
+useEffect(() => {
+        const getUser = async () => {
+
+        try {
+           if(localStorage.getItem('role') === 'user' && localStorage.getItem('user_id') !== undefined){
+                const user_id = localStorage.getItem('user_id');
+                const res = await axios.get(`http://ec2-52-65-40-9.ap-southeast-2.compute.amazonaws.com:8080/users/select/${user_id}`, {
+                	headers: {
+                    		"Content-Type": "application/json",
+                    		"Authorization": `Bearer ${authToken}`,
+                	},
+                });
+
+                if(res.status === 200){
+            		setUserData(res.data)
+            	}
+            } else {
+
+		const res = await axios.get("http://ec2-52-65-40-9.ap-southeast-2.compute.amazonaws.com:8080/users", {
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+
+                if(res.status === 200){
+			setUserData(res.data);
+                }
+
+	    }
+
+            	setMessage("User Data Retrieved");
+
+	    } catch (err) {
+                if (err.response && err.response.status === 500) {
+                    setMessage("There was a problem with the server");
+                } else {
+                    setMessage(err.response?.data?.msg || "Error fetching user data");
+                }
+            }
+        };
+        getUser();
+    }, []);
+
     // File input change
     const onChange = (e) => {
         setFile(e.target.files[0]);
-        let filename = file.name;
     };
 
     return (
         <div className="App">
+	    <h1>Video Transcoder</h1>
+	    <h2>Logged in as Name </h2>
+	    <div className = 'user-container'>
+
+		{isAdmin ? (
+                <table>
+                    <thead>
+                        <tr>
+                            <th>User data overview</th>
+                        </tr>
+                        <tr>
+                        <td>ID</td><td>Email</td><td>First Name</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {userData.map((data, index) => (
+                            <tr key={index}>
+                                <td>{data._id}</td>
+                                <td>{data.email}</td>
+                                <td>{data.firstName}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            ) : (
+                <table>
+		     <tr>
+			 <td>Email</td> <td>{userData.email}</td>
+		     </tr>
+		</table>
+            )}
+	    </div>
             <div className='upload-container'>
-                <h1>Video transcode</h1>
                 <h2>Upload a video file</h2>
-                <h5>Supported formats: ?</h5>
+                <h5>Supported formats: MP4, AVI, MOV, WebM, etc.</h5>
                 <form onSubmit={onSubmit}>
                     <div className="upload-file">
                         <input
                             type="file"
                             className="file-input"
                             id="customFile"
-                            accept="video"
+                            accept="video/*"
                             onChange={onChange}
                             required
                         />
@@ -121,8 +196,9 @@ function Upload() {
 
                 {message && <p>{message}</p>}
             </div>
-
+		<br></br>
             <div className="uploads-container">
+		<h2>Uploaded Files</h2>
                 <table>
                     <thead>
                         <tr>

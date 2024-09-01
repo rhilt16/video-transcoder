baff36d18ac33eae1adba89fc920dfdf96c23c87
rhilt16 <162086587+rhilt16@users.noreply.github.com>
2024-09-01 21:05:02 +1000

    Comments login.js

--- client/src/login.js
+++ client/src/login.js
@@ -4,7 +4,9 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function Login() {
+    // The message useState used for error testing and development
     const [message, setMessage] = useState("Login");
+    // Navigate used for redirecting to the uploads page upon successful login
     const navigate = useNavigate();
     if(localStorage.getItem('authToken')){
         localStorage.removeItem('authToken');
@@ -12,23 +14,24 @@ function Login() {
         localStorage.removeItem('user_id');
     }
 
-    // Create OnSubmit function
+    // Handles the login submit
     const onSubmit = async (e) => {
         e.preventDefault();
+	// Get the login data from the form and append it to formData 
         const email = document.getElementById("emailInput").value;
         const password = document.getElementById("passwordInput").value;
         const formData = new FormData();
         formData.append('email', email);
         formData.append('password', password);
-        console.log(formData);
+        
         try {
-
+	   // Send login request, and store the response in the necessary parts
            const res = await axios.post("http://ec2-52-65-40-9.ap-southeast-2.compute.amazonaws.com:8080/users/login", formData);
            const authToken = res.data.authenticationToken;
            const user_id = res.data.payload.user_id;
            const role = res.data.payload.role;
+	   // Set the success message, and store the response parts in local storage for later use
            setMessage("successfully authenticated");
-           navigate('/upload');
 	   if(localStorage.getItem('authToken')){
               localStorage.removeItem('authToken');
               localStorage.removeItem('user_id');
@@ -37,7 +40,10 @@ function Login() {
            localStorage.setItem('authToken', authToken);
            localStorage.setItem('user_id', user_id);
            localStorage.setItem('role', role);
+	   // Once login is complete, navigate to next page
+	   navigate('/upload');
         } catch (err) {
+	    // Handle errors
             if (err.response && err.response.status === 500) {
                 setMessage("There was a problem with the server");
             } else {
@@ -45,7 +51,7 @@ function Login() {
             }
         }
     };
-
+    // Basic login page
     return (
         <div className="App">
 
